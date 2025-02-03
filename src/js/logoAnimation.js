9463b451c892966508b99ccbb6999aa8f4225333
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-03 17:27:19 +0100

    html/scss: Finished homepage + responsive (only nav left)

---
+++ src/js/logoAnimation.js
@@ -0,0 +1,11 @@
+const logo = document.querySelector(".logo a");
+const letters = logo.textContent.split("");
+
+logo.innerHTML = "";
+console.log(logo);
+for (let i = 0; i <= letters.length - 1; i++) {
+  logo.innerHTML += `<span class="letter letter-${i}">${letters[i]}</span>`;
+  document.querySelector(`.letter-${i}`).style.animation = `0.5s ease ${
+    2 + i / 30
+  }s forwards logo-animation`;
+}
