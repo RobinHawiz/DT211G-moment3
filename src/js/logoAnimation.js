06e59aa7ed6e736423538c3f567c0c11d0440425
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-04 15:14:55 +0100

    js: Logo animation now has different delays on different pages

--- src/js/logoAnimation.js
+++ src/js/logoAnimation.js
@@ -1,10 +1,21 @@
 const logo = document.querySelector(".logo a");
 const letters = logo.textContent.split("");
 
-logo.innerHTML = "";
-for (let i = 0; i <= letters.length - 1; i++) {
-  logo.innerHTML += `<span class="letter letter-${i}">${letters[i]}</span>`;
-  document.querySelector(`.letter-${i}`).style.animation = `0.5s ease ${
-    2 + i / 30
-  }s forwards logo-animation`;
+var path = window.location.pathname;
+var page = path.split("/").slice(-2)[0];
+
+if (page === "sass") {
+  logoAnimation(0);
+} else {
+  logoAnimation(2);
+}
+
+function logoAnimation(delay) {
+  logo.innerHTML = "";
+  for (let i = 0; i <= letters.length - 1; i++) {
+    logo.innerHTML += `<span class="letter letter-${i}">${letters[i]}</span>`;
+    document.querySelector(`.letter-${i}`).style.animation = `0.5s ease ${
+      delay + i / 30
+    }s forwards logo-animation`;
+  }
 }
