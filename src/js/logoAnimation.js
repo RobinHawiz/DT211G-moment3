505008d868e8959db05fbfad71c0e72364d04478
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-04 12:20:47 +0100

    refactor: Removed console.log()

--- src/js/logoAnimation.js
+++ src/js/logoAnimation.js
@@ -2,7 +2,6 @@ const logo = document.querySelector(".logo a");
 const letters = logo.textContent.split("");
 
 logo.innerHTML = "";
-console.log(logo);
 for (let i = 0; i <= letters.length - 1; i++) {
   logo.innerHTML += `<span class="letter letter-${i}">${letters[i]}</span>`;
   document.querySelector(`.letter-${i}`).style.animation = `0.5s ease ${
