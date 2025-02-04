c2ff9d4a2bd9a68552db55a7c156373f71999647
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-04 15:33:09 +0100

    html/webpack: Added a new unfinished page called sass

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,5 +1,5 @@
 const { populateHtmlPlugins } = require("../utils/multipage.js");
-const pages = populateHtmlPlugins(["index"]);
+const pages = populateHtmlPlugins(["index", "sass"]);
 const path = require("path");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
