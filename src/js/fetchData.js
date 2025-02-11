a242d3ac08eb154b7ae0545bb805d9b448c61017
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-11 17:43:42 +0100

    doc: The module fetchData and its functions have been documented

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,3 +1,15 @@
+/**
+ * Fetch data module
+ * @module fetchData
+ */
+
+/**
+ *
+ * @param {string} url url that links to a (publicly accessible) JSON data source.
+ * @returns {Promise} Promise object that contains the reason why the promise was rejected.
+ * @returns {Object | Array} Object or Array that contains some sort of data that was fetched and parsed from the url source.
+ */
+
 export async function fetchData(url) {
   let response = await fetch(url)
     .then((response) => {
