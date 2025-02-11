11fbef0c475b45f7e99ba4ec88b5905515b6175f
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-11 10:54:07 +0100

    js: Refactored the func createBarChart and put it into charts.js

---
+++ src/js/charts.js
@@ -0,0 +1,76 @@
+import Chart from "chart.js/auto";
+export function createBarChart(dataArr, label, barChart) {
+  let labels = [];
+  let data = [];
+  for (let i = 0; i < dataArr.length; i++) {
+    labels.push(dataArr[i][1].split(" "));
+    data.push(dataArr[i][3]);
+  }
+  new Chart(barChart, {
+    type: "bar",
+    data: {
+      labels,
+      datasets: [
+        {
+          label,
+          data,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(255, 205, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(201, 203, 207, 0.2)",
+          ],
+          borderColor: [
+            "rgb(255, 99, 132)",
+            "rgb(255, 159, 64)",
+            "rgb(255, 205, 86)",
+            "rgb(75, 192, 192)",
+            "rgb(54, 162, 235)",
+            "rgb(153, 102, 255)",
+            "rgb(201, 203, 207)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        tooltip: {
+          titleFont: {
+            size: () => {
+              if (window.innerWidth > 610) {
+                console.log("14");
+                return 14;
+              } else if (window.innerWidth > 390) {
+                console.log("10");
+                return 12;
+              } else {
+                return 10;
+              }
+            },
+          },
+          callbacks: {
+            title: (context) => {
+              return context[0].label.replaceAll(",", " ");
+            },
+          },
+        },
+      },
+      scales: {
+        x: {
+          display: () => {
+            return window.innerWidth > 630 ? true : false;
+          },
+        },
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
