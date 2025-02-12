0113e1b384ff345b93b7fe54b465de2a57b79e55
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-12 15:39:06 +0100

    doc: The module createCharts and its functions have been documented

--- src/js/modules/createCharts.js
+++ src/js/modules/createCharts.js
@@ -1,3 +1,8 @@
+/**
+ * Create charts module
+ * @module createCharts
+ */
+
 import {
   Chart,
   BarController,
@@ -20,6 +25,10 @@ Chart.register(
   Tooltip,
   Legend
 );
+/**
+ * Available background colors that can be used by Chart objects.
+ * @type {Array<string>}
+ */
 const backgroundColors = [
   "rgba(255, 99, 132, 0.2)",
   "rgba(255, 159, 64, 0.2)",
@@ -29,6 +38,10 @@ const backgroundColors = [
   "rgba(153, 102, 255, 0.2)",
   "rgba(201, 203, 207, 0.2)",
 ];
+/**
+ * Available border colors that can be used by Chart objects.
+ * @type {Array<string>}
+ */
 const borderColors = [
   "rgb(255, 99, 132)",
   "rgb(255, 159, 64)",
@@ -38,7 +51,11 @@ const borderColors = [
   "rgb(153, 102, 255)",
   "rgb(201, 203, 207)",
 ];
-
+/**
+ * Creates default configuration parameters that gets used by all Chart objects.
+ * @param {Array<ConvertedMiunData>} dataArr specs -> {@link module:convertData}.
+ * @returns {{Array<string>, Array<string>, Array<string>, Array<string>}} an object consisting of four arrays: labels contains the names of a course or program, data contains the total amount of applicants, backgroundColor contains the background colors that will be used by the Chart object, borderColor contains the border colors that will be used by the Chart object.
+ */
 function chartConfig(dataArr) {
   let labels = [];
   let data = [];
@@ -52,7 +69,12 @@ function chartConfig(dataArr) {
   }
   return { labels, data, backgroundColor, borderColor };
 }
-
+/**
+ * Creates a new Chart object of type bar (bar chart). Specs -> {@link https://www.chartjs.org/docs/latest/api/classes/Chart.html|Class: Chart<TType, TData, TLabel>}
+ * @param {Array<ConvertedMiunData>} dataArr Contains data that we want to display in our rendered charts. Specs -> {@link module:convertData}.
+ * @param {string} label label for the dataset parameter in a Chart object.
+ * @param {HTMLElement} barChart HTML canvas element where the chart will be rendered.
+ */
 export function createBarChart(dataArr, label, barChart) {
   const { labels, data, backgroundColor, borderColor } = chartConfig(dataArr);
   new Chart(barChart, {
@@ -105,6 +127,12 @@ export function createBarChart(dataArr, label, barChart) {
     },
   });
 }
+/**
+ * Creates a new Chart object of type pie (pie chart). Specs -> {@link https://www.chartjs.org/docs/latest/api/classes/Chart.html|Class: Chart<TType, TData, TLabel>}
+ * @param {Array<ConvertedMiunData>} dataArr Contains data that we want to display in our rendered charts. Specs -> {@link module:convertData}.
+ * @param {string} label label for the dataset parameter in a Chart object.
+ * @param {HTMLElement} barChart HTML canvas element where the chart will be rendered.
+ */
 export function createPieChart(dataArr, label, pieChart) {
   const { labels, data, backgroundColor, borderColor } = chartConfig(dataArr);
   new Chart(pieChart, {
