66a69dd89bdd96d19b34b4d345bc1ea07e434c66
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-12 14:55:03 +0100

    doc: Minor comment change regarding a param for func sortMiunStatsData

--- src/js/modules/sortData.js
+++ src/js/modules/sortData.js
@@ -5,7 +5,7 @@
 
 /**
  * Sorts a given array with ConvertedMiunData objects as elements in descending order by the total number of applicants.
- * @param {Array<ConvertedMiunData>} data see typedef in {@link module:convertData}.
+ * @param {Array<ConvertedMiunData>} data specs -> {@link module:convertData}.
  */
 export function sortMiunStatsData(data) {
   data.sort((a, b) => b[3] - a[3]);
