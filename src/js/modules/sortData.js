183f2a5cd488a16a7645ac80a8e0acf9d85b76a6
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-12 14:23:59 +0100

    doc: The module sortData and its functions have been documented

--- src/js/modules/sortData.js
+++ src/js/modules/sortData.js
@@ -1,4 +1,12 @@
+/**
+ * Convert data module
+ * @module sortData
+ */
+
+/**
+ * Sorts a given array with ConvertedMiunData objects as elements in descending order by the total number of applicants.
+ * @param {Array<ConvertedMiunData>} data see typedef in {@link module:convertData}.
+ */
 export function sortMiunStatsData(data) {
-  // Descending order (we compare the total amount of applicants)
   data.sort((a, b) => b[3] - a[3]);
 }
