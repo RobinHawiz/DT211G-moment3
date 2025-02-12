834981a2845e565777d65146e5465f78a7e2a426
RobinH <Robin.Hawiz.6670@student.uu.se>
2025-02-12 12:18:32 +0100

    doc: The module convertData and its functions have been documented

--- src/js/modules/convertData.js
+++ src/js/modules/convertData.js
@@ -1,5 +1,33 @@
-// Note to self: Create @type {{param1: type, param2: type}} data
+/**
+ * Convert data module
+ * @module convertData
+ */
 
+/**
+ * Specific JSON data coming from mid sweden university that has been parsed by {@link module:fetchData}.
+ * @typedef MiunData
+ * @type {Object}
+ * @property {string} type indicates whether the data represents a course or a program
+ * @property {string} name name of the course or program
+ * @property {string} applicantsFirstHand total amount of first hand applicants
+ * @property {string} applicants total amount of applicants
+ */
+
+/**
+ * {@link MiunData} object converted into an array.
+ * @typedef ConvertedMiunData
+ * @type {array}
+ * @property {string} 0 - indicates whether the data represents a course or a program
+ * @property {string} 1 - name of the course or program
+ * @property {string} 2 - total amount of first hand applicants
+ * @property {string} 3 - total amount of applicants
+ */
+
+/**
+ * Converts MiunData to ConvertedMiunData and separates it into two distinct arrays based on their type (course or program).
+ * @param {Array<MiunData>} data see definition for {@link MiunData}.
+ * @returns {{Array<ConvertedMiunData>, Array<ConvertedMiunData>}} an object consisting of two arrays: the first contains all ConvertedMiunData with the type 'Kurs' (course), and the second contains those with the type 'Program'.
+ */
 export function convertMiunStatsData(data) {
   let dataArrCourses = [];
   let dataArrPrograms = [];
@@ -8,7 +36,7 @@ export function convertMiunStatsData(data) {
       // We convert objects into arrays in order to be able to utilize array methods later on.
       dataArrCourses.push([...Object.values(obj)]);
     }
-    // object[type] === "Program", probably...
+    // obj[type] === "Program", probably...
     else {
       dataArrPrograms.push([...Object.values(obj)]);
     }
